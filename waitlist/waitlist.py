e65dc2f3e7897940ab15520b1651afe2195e5a21
Heng Zheng Kai <zkheng.2023@scis.smu.edu.sg>
2025-03-14 19:19:52 +0800

    updated with mongodb config

--- waitlist/waitlist.py
+++ waitlist/waitlist.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from flask_mongoengine import MongoEngine
+from os import environ
+import os
 
 app = Flask(__name__)
 
 CORS(app)
+
+app.config['MONGODB_SETTINGS'] = {
+    'host': os.getenv('MONGO_URI')  # Set this in your .env file
+}
