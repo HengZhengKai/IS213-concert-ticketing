d37d15adc6b3e1bce2f4e0bff1ff664888e9a64a
Heng Zheng Kai <zkheng.2023@scis.smu.edu.sg>
2025-03-31 22:53:45 +0800

    Update check_in_ticket.py

--- composite-microservices/check_in_ticket/check_in_ticket.py
+++ composite-microservices/check_in_ticket/check_in_ticket.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 import os, sys
 import qrcode
 import uuid
 import requests
+from io import BytesIO
 from invokes import invoke_http
 
 app = Flask(__name__)
@@ -32,8 +33,34 @@ def is_ticket_checked_in(ticketID):
 
 @app.route("/generateqr/<string:ticketID>", methods=['POST'])
 def generate_qr(ticketID):
-    pass
+    if is_ticket_checked_in(ticketID) == True:
+        return jsonify({"error": "Ticket already checked in"}), 400
+    
+    # Generate QR code
+    qr = qrcode.QRCode(version=1, box_size=10, border=5)
+    qr.add_data(ticketID)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill="black", back_color="white")
+
+    # Save QR code to an in-memory file
+    img_io = BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png')
 
 @app.route("/scanqr/<string:ticketID>", methods=['POST'])
 def on_qr_scanned(ticketID):
-    pass
+    if is_ticket_checked_in(ticketID):
+        return jsonify({"error": "Ticket already checked in"}), 400
+
+    print('\n-----Invoking ticket microservice-----')
+    response = invoke_http(f"{ticket_URL}/{ticketID}", method='PUT', json={"isCheckedIn": True})
+
+    if isinstance(response, dict) and response.get("code") == 200:
+        return True  # Success
+    return False  # Failed to update
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5103, debug=True)
