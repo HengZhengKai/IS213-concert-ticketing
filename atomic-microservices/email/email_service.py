073ce2f8bdbbe2c64167f5b62234e5334cc89fd2
breann0ng <breann.ong.2023@scis.smu.edu.sg>
2025-04-07 11:30:26 +0800

    Added Twilio Sendgrid Email API

--- atomic-microservices/email/email_service.py
+++ atomic-microservices/email/email_service.py
@@ -778,4 +778,4 @@ if __name__ == "__main__":
     consumer_thread.start()
     
     # Start Flask app
-    app.run(host='0.0.0.0', port=5007, debug=True)
+    app.run(host='0.0.0.0', port=5008, debug=True)
