e57c7d8c3e1f6b901bd82f668fcded322ae7d617
Heng Zheng Kai <zkheng.2023@scis.smu.edu.sg>
2025-04-08 13:13:03 +0800

    Update buy_ticket.py

--- composite-microservices/buy_ticket/buy_ticket.py
+++ composite-microservices/buy_ticket/buy_ticket.py
@@ -70,7 +70,6 @@ def buy_ticket():
             return jsonify(result), result["code"]
 
         except Exception as e:
-            # Unexpected error in code
             exc_type, exc_obj, exc_tb = sys.exc_info()
             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
             ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
@@ -78,7 +77,7 @@ def buy_ticket():
 
             return jsonify({
                 "code": 500,
-                "message": "buy_ticket.py internal error: " + ex_str
+                "message": f"Unexpected error: {str(e)}"
             }), 500
 
 
@@ -89,112 +88,123 @@ def buy_ticket():
     }), 400
 
 def process_buy_ticket(userID, eventName, eventID, eventDateTime, seatNo, seatCategory, price, paymentID):
-    # Step 2-3. Update event availableSeats
-    print('\n-----Invoking event microservice-----')
-    event = invoke_http(f"{event_URL}/{eventID}/{eventDateTime}")
-    availableSeats = event["data"]["availableSeats"]
-    newSeats = availableSeats - 1
-    event_result = invoke_http(f"{event_URL}/{eventID}/{eventDateTime}",method="PUT",
-                               json={"availableSeats":newSeats})
-    
-    # Check the event result; if a failure, do error handling.
-    code = event_result["code"]
-    if code not in range(200, 300):
-        return {
-            "code": 500,
-            "data": {"event_result": event_result},
-            "message": "Failed to update event."
-        }
-    
-    # Step 4-5: Get user name and email from userID
-    print('\n-----Invoking user microservice-----')
-    user = invoke_http(f"{user_URL}/{userID}")
-    userName = user["data"]["name"]
-    userEmail = user["data"]["email"]
-
-    # Step 6-7: Create new ticket
-    print('\n-----Invoking ticket microservice-----')
-    ticketID = None
-    for i in range(5): # max 5 attempts to create new ticket
-        temp_ticketID = "T" + str(uuid.uuid4())[:4]
-        ticket_data = {
-            "ownerID": userID,
-            "ownerName": userName,
-            "eventID": eventID,
-            "eventName": eventName,
-            "eventDateTime": eventDateTime,
-            "seatNo": seatNo,
-            "seatCategory": seatCategory,
-            "price": price,
-            "resalePrice": None,  
-            "status": "paid",
-            "paymentID": paymentID,
-            "isCheckedIn": False
-        }
-
-        ticket_result = invoke_http(f"{ticket_URL}/{temp_ticketID}", method="POST", json=ticket_data)
+    try:
+        # Step 2-3: Update event availableSeats
+        print('\n-----Invoking event microservice-----')
+        event = invoke_http(f"{event_URL}/{eventID}/{eventDateTime}")
         
-        if ticket_result["code"] == 201:
-            ticketID = temp_ticketID
-            break
+        if "data" not in event or "availableSeats" not in event["data"]:
+            return {
+                "code": 500,
+                "message": "Event data not found or unavailable."
+            }
 
-    if ticketID is None:
+        availableSeats = event["data"]["availableSeats"]
+        newSeats = availableSeats - 1
+        event_result = invoke_http(f"{event_URL}/{eventID}/{eventDateTime}", method="PUT", json={"availableSeats": newSeats})
+
+        if event_result["code"] not in range(200, 300):
+            return {
+                "code": 500,
+                "message": "Failed to update event, check event microservice."
+            }
+
+        # Step 4-5: Get user name and email from userID
+        print('\n-----Invoking user microservice-----')
+        user = invoke_http(f"{user_URL}/{userID}")
+        
+        if "data" not in user or "name" not in user["data"] or "email" not in user["data"]:
+            return {
+                "code": 500,
+                "message": "User data not found."
+            }
+
+        userName = user["data"]["name"]
+        userEmail = user["data"]["email"]
+
+        # Step 6-7: Create new ticket
+        print('\n-----Invoking ticket microservice-----')
+        ticketID = None
+        for i in range(5):  # max 5 attempts
+            temp_ticketID = "T" + str(uuid.uuid4())[:4]
+            ticket_data = {
+                "ownerID": userID,
+                "ownerName": userName,
+                "eventID": eventID,
+                "eventName": eventName,
+                "eventDateTime": eventDateTime,
+                "seatNo": seatNo,
+                "seatCategory": seatCategory,
+                "price": price,
+                "resalePrice": None,
+                "status": "paid",
+                "paymentID": paymentID,
+                "isCheckedIn": False
+            }
+
+            ticket_result = invoke_http(f"{ticket_URL}/{temp_ticketID}", method="POST", json=ticket_data)
+            if ticket_result["code"] == 201:
+                ticketID = temp_ticketID
+                break
+
+        if ticketID is None:
+            return {
+                "code": 500,
+                "message": "Failed to create ticket after multiple attempts."
+            }
+
+        # Step 8-9: Create new transaction
+        print('\n-----Invoking transaction microservice-----')
+        transaction_code = None
+        while transaction_code == None or transaction_code == 409:  # rare conflict case
+            transactionID = "Trans" + str(uuid.uuid4())[:7]
+
+            transaction_data = {
+                "transactionID": transactionID,
+                "type": "purchase",
+                "userID": userID,
+                "ticketID": ticketID,
+                "paymentID": paymentID,
+                "amount": price
+            }
+
+            transaction_result = invoke_http(f"{transaction_URL}", method="POST", json=transaction_data)
+            transaction_code = transaction_result["code"]
+
+        # Step 10: Email buyer asynchronously
+        print('\n-----Invoking email service through AMQP-----')
+        payload = {
+            "user_id": userID,
+            "user_name": userName,
+            "user_email": userEmail,
+            "ticket_id": ticketID,
+            "event_id": eventID,
+            "event_name": eventName,
+            "event_date": eventDateTime,
+            "seat_no": seatNo,
+            "seat_category": seatCategory,
+            "price": price
+        }
+
+        channel.basic_publish(
+            exchange="ticketing",
+            routing_key="ticket.purchased",
+            body=json.dumps(payload)
+        )
+
+        return {
+            "code": 201,
+            "data": {
+                "ticketID": ticketID,
+                "transactionID": transactionID
+            },
+            "message": "Ticket purchase successful."
+        }
+    except Exception as e:
         return {
             "code": 500,
-            "message": "Failed to create ticket after multiple attempts. Try again later."
+            "message": f"Error processing ticket: {str(e)}"
         }
-
-    # Step 8-9: Create new transaction
-    print('\n-----Invoking transaction microservice-----')
-    transaction_code = None
-    while transaction_code == None or transaction_code == 409: # ticketid already exists (rare chance)
-        print('\n-----Creating new transaction-----')
-
-        transactionID = "Trans" + str(uuid.uuid4())[:7]
-
-        transaction_data = {
-            "transactionID": transactionID,
-            "type": "purchase",
-            "userID": userID,
-            "ticketID": ticketID,
-            "paymentID": paymentID,
-            "amount": price
-        }
-
-        transaction_result = invoke_http(f"{transaction_URL}", method="POST", json=transaction_data)
-        transaction_code = transaction_result["code"]
-
-    # Step 10: Email buyer asynchronously
-    print('\n-----Invoking email service through AMQP-----')
-    
-    payload = {
-        "user_id": userID,
-        "user_name": userName,
-        "user_email": userEmail,
-        "ticket_id": ticketID,
-        "event_id": eventID,
-        "event_name": eventName,
-        "event_date": eventDateTime,
-        "seat_no": seatNo,
-        "seat_category": seatCategory,
-        "price": price
-    }
-
-    channel.basic_publish(
-        exchange="ticketing",
-        routing_key="ticket.purchased",
-        body=json.dumps(payload)
-    )
-
-    # Step 11: Return
-    return {
-        "code": 201,
-        "data": {
-            "ticketID": ticketID,
-            "transactionID": transactionID
-        },
-        "message": "Ticket purchase successful."
-    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5100, debug=True)
